6784450fdae50b394150a773ea54b0263492a362
Laing91 <86000107+Laing91@users.noreply.github.com>
2023-06-20 22:58:49 +0100

    Added mouseover to create square filling

--- App.js
+++ App.js
@@ -3,4 +3,8 @@ for (let i = 0; i < 16 * 16; i++) {
   const gridItem = document.createElement("div");
   gridItem.classList.add("grid-item");
   gridContainer.appendChild(gridItem);
+
+  gridItem.addEventListener("mouseover", () => {
+    gridItem.style.backgroundColor = "red";
+  });
 }
